fd21ae7ff4fe800a3ebdc7b6094a47201bce05e4
Jake <gamabjake@gmail.com>
2023-03-12 20:54:39 +0800

    project added

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './components/Navbar';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import AuthProvider from './context/auth';
+
+function App() {
+  return (
+    <AuthProvider>
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path='/register' element={<Register/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
